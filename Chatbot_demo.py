638045fe365b41db2cdb16ac794b9cad4a638c07
chicheng86 <55564812+chicheng86@users.noreply.github.com>
2020-07-02 11:10:04 -0400

    Add files via upload
    This is our first Chatbo demo !!!

---
+++ Chatbot_demo.py
@@ -0,0 +1,28 @@
+
+# coding: utf-8
+
+# In[1]:
+
+def introduce_myself():
+    Name=input('What is your name?')
+    print('Nice to meet you, '+Name+'!')
+    Food=input('What is your favorite food ?')
+    print('hmm ~~ I love '+Food +' too ! That is yummy!!')
+    Sports=input('What is your favorite sports? ')
+    print('Wow, that is impressive! I tried to learn '+Sports +' too, but it is too difficult and I gave up..')
+    Feeling=input('How are you feeling today?')
+    print('Why are you feeling '+Feeling,'now?')
+    reason=input('Please tell me..')
+    print('I understand ! Thanks for sharing')
+    print('Goodbye',Name,'I liked chatting with you ! ')
+
+
+# In[2]:
+
+introduce_myself()
+
+
+# In[ ]:
+
+
+
