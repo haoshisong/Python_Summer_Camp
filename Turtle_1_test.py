7009f1f6906f48b3275c0034c6c34192d4862d7c
haoshisong <67443311+haoshisong@users.noreply.github.com>
2020-07-20 11:05:12 -0400

    Add files via upload

---
+++ Turtle_1_test.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import turtle
+hermes=turtle.Turtle()
+hermes.shape('turtle')
+hermes.color('blue')
+hermes.forward(150)
+hermes.right(30)
+hermes.right(30)
+hermes.right(30)
+hermes.right(30)
+hermes.right(30)
+hermes.right(30)
+hermes.backward(100)
+hermes.circle(50)
+hermes.right(90)
+hermes.forward(150)
+hermes.right(30)
+hermes.right(30)
+hermes.right(30)
+hermes.right(30)
+hermes.right(30)
+hermes.right(30)
+hermes.backward(100)
+hermes.circle(50)
+hermes.right(90)
+hermes.forward(150)
+hermes.right(30)
+hermes.right(30)
+hermes.right(30)
+hermes.right(30)
+hermes.right(30)
+hermes.right(30)
+hermes.backward(100)
+hermes.circle(50)
+hermes.right(90)
+hermes.forward(150)
+hermes.right(30)
+hermes.right(30)
+hermes.right(30)
+hermes.right(30)
+hermes.right(30)
+hermes.right(30)
+hermes.backward(100)
+hermes.circle(50)
+hermes.right(90)
+hermes.left(45)
+hermes.forward(340)
+
+
+# In[ ]:
+
+
+
+
