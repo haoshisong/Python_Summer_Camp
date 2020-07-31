4f20355f98d94732d85c22a27c1a2e1871b427f0
haoshisong <67443311+haoshisong@users.noreply.github.com>
2020-07-31 10:23:59 -0400

    Add files via upload

---
+++ even_odd number.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+num = int(input("8 "))  
+if (num % 2) == 0:  
+   print("{0} is Even number".format(num))  
+else:  
+   print("{0} is Odd number".format(num))
+
